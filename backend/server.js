2719447b3e1413372f433d7945983216075c05b9
zhangyutong <zhangyutong@sailyond.com>
2023-11-15 11:34:25 +0000

    final update

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 const app = express();
-const port = 8891;
+const port = 8892;
 
 app.use(cors()); // Activate CORS
 // Create a connection to the database
